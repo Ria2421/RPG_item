a3ae37740df6419c0a3d9f50c347cbdda3b22ebb
S-JOBI23-055\student <231105@st.yoshida-g.ac.jp>
2024-08-30 09:43:09 +0900

    projectの作成。Itemクラスの作成。 #1

---
+++ RPG_Item/Program.cs
@@ -0,0 +1,48 @@
+﻿//--------------------------------------------------
+//
+// メインプログラム [Program.cs]
+// Author:Kenta Nakamoto
+// Data 2024/08/30
+// Update 2024/08/30
+//
+//--------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Item
+{
+    internal class Item
+    {
+        /// <summary>
+        /// アイテムID
+        /// </summary>
+        public int ID {  get; set; }
+
+        /// <summary>
+        /// アイテム名
+        /// </summary>
+        public string Name {  get; set; }
+
+        /// <summary>
+        /// 価格
+        /// </summary>
+        public int Price {  get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item();
+
+            item.Name = "Test";
+
+            Console.WriteLine(item.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
