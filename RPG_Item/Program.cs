18427f4ddda44889b1537b54386da80a1465ba7d
S-JOBI23-055\student <231105@st.yoshida-g.ac.jp>
2024-08-30 10:03:51 +0900

    #3 所持金・販売アイテム情報を表示

--- RPG_Item/Program.cs
+++ RPG_Item/Program.cs
@@ -32,10 +32,30 @@ namespace RPG_Item
         public int Price {  get; set; }
     }
 
+    internal class Player
+    {
+        /// <summary>
+        /// 所持アイテム
+        /// </summary>
+        public List<Item> HaveItems { get; set; }
+
+        /// <summary>
+        /// 所持金
+        /// </summary>
+        public int HaveMoney { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // プレイヤー作成
+            Player player = new Player()
+            {
+                // 所持金の設定
+                HaveMoney = 1000
+            };
+
             // 道具屋のアイテムリスト
             List<Item> shopList = new List<Item>()
             {
@@ -43,6 +63,25 @@ namespace RPG_Item
                 new Item() { ID = 2, Name = "どうのつるぎ", Price = 500},
                 new Item() { ID = 3, Name = "やくそう", Price = 100}
             };
+
+            while (true)
+            {
+                // 所持金の表示
+                Console.WriteLine("プレイヤーの所持金" + player.HaveMoney + "$" + "\n");
+
+                // アイテムリストの表示
+                Console.WriteLine("道具屋のアイテム一覧");
+                Console.WriteLine("========================================");
+                foreach (Item item in shopList)
+                {
+                    Console.WriteLine(item.ID.ToString() + "：" + item.Name + "  " + item.Price + "$" );
+                }
+                Console.WriteLine("========================================");
+
+                // 入力処理
+                var input = Console.ReadLine();
+                if (input == "0") { break; }    // 0入力で終了
+            }
         }
     }
 }
